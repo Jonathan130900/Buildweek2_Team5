89f6e7515d53e924f6754ed21b64139146cca3b5
giacomo3008 <giacomorigo7@gmail.com>
2024-12-16 15:03:43 +0100

    add albums

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const heartIcons = document.getElementsByClassName("heart");
+
+console.log(heartIcons.array);
+
+for(let i=0; i<heartIcons.length; i++){
+    heartIcons[i].addEventListener("mouseover", () => {
+        heartIcons[i].classList.remove("bi-heart");
+        heartIcons[i].classList.add("bi-heart-fill");
+    });
+
+    heartIcons[i].addEventListener("mouseout", () => {
+        heartIcons[i].classList.remove("bi-heart-fill");
+        heartIcons[i].classList.add("bi-heart");
+    });
+}
+
