c4fc1063b7b920c417927bf089f2a44ab8f1e335
giacomo3008 <giacomorigo7@gmail.com>
2024-12-18 14:12:34 +0100

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
 const heartIcons = document.getElementsByClassName("heart");
+const firstCards = document.querySelectorAll(".first-cards");
 const ALBUM_URL = "https://striveschool-api.herokuapp.com/api/deezer/album/";
 const TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzViMmQ1ZGQyMjA3MTAwMTVkZTJmMWYiLCJpYXQiOjE3MzQ0Njg0NjEsImV4cCI6MTczNTY3ODA2MX0.Q89cp9cHweU9w5ZWnXYvoPa9mO26MvOv3dP8Qzmkusc";
 
-const array = [];
+const arrAlbums = [];
 
-for(let i=0; i<heartIcons.length; i++){
+
+
+class Albums {
+    constructor(_title, _cover_big, _label, _tracklistURL) {
+        this.title = _title;
+        this.cover_big = _cover_big;
+        this.label = _label;
+        this.tracklistURL = _tracklistURL;
+    }
+}
+
+for (let i = 0; i < heartIcons.length; i++) {
     heartIcons[i].addEventListener("mouseover", () => {
         heartIcons[i].classList.remove("bi-heart");
         heartIcons[i].classList.add("bi-heart-fill");
@@ -16,27 +28,28 @@ for(let i=0; i<heartIcons.length; i++){
     });
 }
 
-window.onload = function(){
+window.onload = function () {
     randomAlbum();
 }
 
-async function randomAlbum(){
-    let i=0;
-    for(let i=0; i<5; i++){
+async function randomAlbum() {
+    let i = 0;
+    for (let i = 0; i < 17; i++) {
         let id = Math.floor(Math.random() * 50) + 500000;
         try {
-            const res = await fetch(ALBUM_URL + id, {
+            const result = await fetch(ALBUM_URL + id, {
                 headers: {
                     authorization: TOKEN,
                 },
             });
 
-            if (res.ok) {
-                const data = await res.json();
-                if(data.id){
-                    console.log("album id: ",data.id);
-                    array.push(data);
-                }else{
+            if (result.ok) {
+                const data = await result.json();
+                if (data.id) {
+                    console.log("album id: ", data.id);
+                    let album = new Albums(data.title, data.cover_big, data.label, data.tracklist);
+                    arrAlbums.push(album);
+                } else {
                     i--;
                     console.log("id non valido");
                 }
@@ -48,6 +61,8 @@ async function randomAlbum(){
             console.log("Errore durante la richiesta:", err);
         }
     }
-    console.log(array);
+    console.log(arrAlbums);
 }
+
+
 
