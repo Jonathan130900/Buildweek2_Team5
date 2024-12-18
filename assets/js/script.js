ac5b4ccf3b563d3da5a1edabe83d6301840316a5
giacomo3008 <giacomorigo7@gmail.com>
2024-12-18 09:59:27 +0100

    aggiunta api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const heartIcons = document.getElementsByClassName("heart");
+const ALBUM_URL = "https://striveschool-api.herokuapp.com/api/deezer/album/";
+const TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzViMmQ1ZGQyMjA3MTAwMTVkZTJmMWYiLCJpYXQiOjE3MzQ0Njg0NjEsImV4cCI6MTczNTY3ODA2MX0.Q89cp9cHweU9w5ZWnXYvoPa9mO26MvOv3dP8Qzmkusc";
 
-console.log(heartIcons.array);
+const array = [];
 
 for(let i=0; i<heartIcons.length; i++){
     heartIcons[i].addEventListener("mouseover", () => {
@@ -12,5 +14,40 @@ for(let i=0; i<heartIcons.length; i++){
         heartIcons[i].classList.remove("bi-heart-fill");
         heartIcons[i].classList.add("bi-heart");
     });
+}
+
+window.onload = function(){
+    randomAlbum();
+}
+
+async function randomAlbum(){
+    let i=0;
+    for(let i=0; i<5; i++){
+        let id = Math.floor(Math.random() * 50) + 500000;
+        try {
+            const res = await fetch(ALBUM_URL + id, {
+                headers: {
+                    authorization: TOKEN,
+                },
+            });
+
+            if (res.ok) {
+                const data = await res.json();
+                if(data.id){
+                    console.log("album id: ",data.id);
+                    array.push(data);
+                }else{
+                    i--;
+                    console.log("id non valido");
+                }
+            } else {
+                i--;
+                throw new Error('Error getting album');
+            }
+        } catch (err) {
+            console.log("Errore durante la richiesta:", err);
+        }
+    }
+    console.log(array);
 }
 
