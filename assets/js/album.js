4362d258e85711114a14fcc7a0945202976a2dde
Jonathan130900 <jlb130900@gmail.com>
2024-12-19 10:36:58 +0100

    Artist page WIP

--- assets/js/album.js
+++ assets/js/album.js
@@ -23,6 +23,42 @@
 //     };
 //   }
 // });
+
+/*
+function fetchAlbumData() {
+  const albumId = "YOUR_ALBUM_ID";
+  const apiUrl = `https://api.deezer.com/album/${albumId}`;
+
+  if (sessionStorage.getItem("albumAccessed")) {
+    alert("You have already accessed this album. Reloading the page...");
+    window.location.href = "index.html";
+    return;
+  }
+
+  sessionStorage.setItem("albumAccessed", "true");
+
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch album data");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      document.querySelector(".titleAlbum").innerText = data.title;
+      document.querySelector(".bandPfp").src = data.artist.picture_small;
+    })
+    .catch((error) => {
+      console.error("Error fetching album data:", error);
+      alert(
+        "Something went wrong while fetching album data. Please try again later."
+      );
+    });
+}
+
+fetchAlbumData();
+*/
+
 // Recupera i dati dell'album dal sessionStorage
 const albumCover = document.getElementById("album-cover");
 const colorThief = new ColorThief();
