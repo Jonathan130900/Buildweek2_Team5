7967e22196990871dc173f25d3cf9e4fd53b61b9
Domy15 <domenico2016dellaventura@gmail.com>
2024-12-19 11:12:49 +0100

    commit

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,5 +1,5 @@
 const url = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
-const TOKEN = "Bearer YOUR_TOKEN_HERE";
+const TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzViMmQ1ZGQyMjA3MTAwMTVkZTJmMWYiLCJpYXQiOjE3MzQ0Njg0NjEsImV4cCI6MTczNTY3ODA2MX0.Q89cp9cHweU9w5ZWnXYvoPa9mO26MvOv3dP8Qzmkusc";
 const searchBar = document.getElementById("searchBar");
 const form = document.getElementById("form");
 const showSong = document.getElementById("showSong");
@@ -7,6 +7,15 @@ const playIcon = document.getElementById("playIcon");
 const mobilePlayIcon = document.getElementById("player-icon2");
 let currentAudio = null;
 let isPlaying = false;
+let songList = [];
+let currentSongIndex = 0;
+const volumeBar = document.querySelector(".progress-bar");
+
+volumeBar.addEventListener("input", function() {
+    if (currentAudio) {
+        currentAudio.volume = volumeBar.value;
+    }
+});
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -22,7 +31,8 @@ async function searchSong() {
         });
         if (response.ok) {
             const data = await response.json();
-            printSong(data.data);
+            songList = data.data;
+            printSong(songList);
         } else {
             console.log("Errore nella risposta: ", response.status);
         }
@@ -48,18 +58,21 @@ function printSong(data) {
                 </div>
             </div>`;
     }
+
     const songImages = document.querySelectorAll('.tagStart img');
     songImages.forEach(img => {
-        img.addEventListener('click', function() {
+        img.addEventListener('click', function () {
             const audioUrl = img.getAttribute('data-song');
             const songTitle = img.nextElementSibling.querySelector('h4').textContent;
             const artistName = img.nextElementSibling.querySelector('p').textContent;
             const albumCover = img.src;
+
             document.getElementById("songTitle").textContent = songTitle;
             document.getElementById("artistName").textContent = artistName;
             document.getElementById("songCover").src = albumCover;
             document.querySelector("#playerSection .song-cover").src = albumCover;
             document.querySelector("#playerSection .song-title-container h6").innerHTML = `${songTitle} - ${artistName}`;
+
             if (currentAudio && !currentAudio.paused) {
                 currentAudio.pause();
                 currentAudio.currentTime = 0;
@@ -71,6 +84,9 @@ function printSong(data) {
             currentAudio.play();
             isPlaying = true;
             updatePlayIcons();
+            currentSongIndex = Array.from(songImages).indexOf(img);
+            console.log(currentSongIndex);
+            
         });
     });
 }
@@ -85,7 +101,7 @@ function updatePlayIcons() {
     }
 }
 
-playIcon.addEventListener("click", function() {
+playIcon.addEventListener("click", function () {
     if (currentAudio) {
         if (isPlaying) {
             currentAudio.pause();
@@ -99,7 +115,7 @@ playIcon.addEventListener("click", function() {
     }
 });
 
-mobilePlayIcon.addEventListener("click", function() {
+mobilePlayIcon.addEventListener("click", function () {
     if (currentAudio) {
         if (isPlaying) {
             currentAudio.pause();
@@ -112,3 +128,46 @@ mobilePlayIcon.addEventListener("click", function() {
         }
     }
 });
+
+function nextSong() {
+    currentSongIndex++;
+    if (currentSongIndex >= songList.length) {
+        currentSongIndex = 0;
+    }
+    playSongAtIndex(currentSongIndex);
+}
+
+function prevSong() {
+    currentSongIndex--;
+    if (currentSongIndex < 0) {
+        currentSongIndex = songList.length - 1;
+    }
+    playSongAtIndex(currentSongIndex);
+}
+
+function playSongAtIndex(index) {
+    const song = songList[index];
+    const audioUrl = song.preview;
+    const songTitle = song.title;
+    const artistName = song.artist.name;
+    const albumCover = song.album.cover;
+
+    document.getElementById("songTitle").textContent = songTitle;
+    document.getElementById("artistName").textContent = artistName;
+    document.getElementById("songCover").src = albumCover;
+    document.querySelector("#playerSection .song-cover").src = albumCover;
+    document.querySelector("#playerSection .song-title-container h6").innerHTML = `${songTitle} - ${artistName}`;
+
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(audioUrl);
+    currentAudio.play();
+    isPlaying = true;
+    updatePlayIcons();
+}
+
+document.querySelector('.bi-skip-start-fill').addEventListener("click", prevSong);
+document.querySelector('.bi-skip-end-fill').addEventListener("click", nextSong);
