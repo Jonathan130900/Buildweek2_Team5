d312ffa466d42679208b5bda2269736e9416a412
giacomo3008 <giacomorigo7@gmail.com>
2024-12-19 12:27:39 +0100

    aggiunta artisti

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,11 @@ const cards = document.getElementsByClassName("card-button");
 
 
 const ALBUM_URL = "https://striveschool-api.herokuapp.com/api/deezer/album/";
+const ARTIST_URL = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
 const TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzViMmQ1ZGQyMjA3MTAwMTVkZTJmMWYiLCJpYXQiOjE3MzQ0Njg0NjEsImV4cCI6MTczNTY3ODA2MX0.Q89cp9cHweU9w5ZWnXYvoPa9mO26MvOv3dP8Qzmkusc";
-const arrAlbums = [];
-let arrFourAlbums = [];
-let arrSixAlbums = [];
+let arrAlbums = [];
+let arrArtists = [];
+let arrTracks = [];
 
 class Albums {
     constructor(_title, _cover_big, _label, _tracklistURL, _artist, _year, _nbTracks, _duration) {
@@ -24,6 +25,23 @@ class Albums {
         this.artist = _artist;
         this.year = String(_year).substring(0, 4);
         this.nbTracks = _nbTracks;
+        this.duration = _duration;
+    }
+}
+class Artists {
+    constructor(_name, _picture_big, _nb_fan, _tracklistURL) {
+        this.name = _name;
+        this.picture_big = _picture_big;
+        this.nb_fan = _nb_fan;
+        this.tracklistURL = _tracklistURL;
+    }
+}
+
+class Tracks {
+    constructor(_title, _cover_big, _rank, _duration) {
+        this.title = _title;
+        this.cover_big = _cover_big;
+        this.rank = _rank;
         this.duration = _duration;
     }
 }
@@ -41,37 +59,58 @@ for (let i = 0; i < heartIcons.length; i++) {
 }
 
 window.onload = async function () {
-    await randomAlbum();
-    await printSecondCards();
-    main.classList.remove("d-none");
-    skeleton.classList.add("d-none");
-    btnPlay.addEventListener("click", () => handleAlbumClick(arrAlbums[10]));
-    listenersBtn();
-}
-async function printSecondCards() {
-    for (let i = 0; i < 6; i++) {
-        arrFourAlbums = [];
-        await randomFourAlbums();
+    // Controlla se il sito è stato già caricato
+    const isFirstLoad = sessionStorage.getItem("isFirstLoad");
 
-        const imgs = secondCards[i].getElementsByTagName("img");
-        for (let j = 0; j < imgs.length; j++) {
-            const linkImg = arrFourAlbums[j].cover_big;
-            imgs[j].src = linkImg;
-        }
+    if (!isFirstLoad) {
+        // Esegui le funzioni solo la prima volta
+        await randomAlbum();
+        await randomArtists();
+        main.classList.remove("d-none");
+        skeleton.classList.add("d-none");
+        btnPlay.addEventListener("click", () => handleAlbumClick(arrAlbums[10]));
+        listenersBtn();
 
-        const title = secondCards[i].getElementsByTagName("p")[0];
-        const titleAlbum = arrFourAlbums[0].title;
-        title.innerText = titleAlbum;
-        arrSixAlbums[i] = arrFourAlbums[0];
+        //carica in sessionStorage gli array
+        sessionStorage.setItem("isFirstLoad", "true");
+        sessionStorage.setItem("arrAlbums", JSON.stringify(arrAlbums));
+        sessionStorage.setItem("arrArtists", JSON.stringify(arrArtists));
+        sessionStorage.setItem("arrTracksArtists", JSON.stringify(arrTracks));
+    } else {
+        console.log("Il sito è stato già caricato. Niente funzioni iniziali.");
+        arrAlbums = JSON.parse(sessionStorage.getItem("arrAlbums"));
+        arrArtists = JSON.parse(sessionStorage.getItem("arrArtists"));
+        arrTracks = JSON.parse(sessionStorage.getItem("arrTracksArtists"));
+        printCards();
+        printSecondCards();
+        main.classList.remove("d-none");
+        skeleton.classList.add("d-none");
+        btnPlay.addEventListener("click", () => handleAlbumClick(arrAlbums[10]));
+        listenersBtn();
     }
 }
 
-async function randomFourAlbums() {
-    let i = 0;
-    for (let i = 0; i < 4; i++) {
-        let id = Math.floor(Math.random() * 150) + 700000;
+function printSecondCards() {
+    for (let i = 0; i < 6; i++) {
+        const imgs = secondCards[i].getElementsByTagName("img");
+        for (let j = 0; j < imgs.length; j++) {
+            if(arrTracks[i][j]){
+                const linkImg = arrTracks[i][j].cover_big;
+                imgs[j].src = linkImg;
+            }
+        }
+
+        const name = secondCards[i].getElementsByTagName("p")[0];
+        const nameArtist = arrArtists[i].name;
+        name.innerText = nameArtist;
+    }
+}
+
+async function randomArtists() {
+    for (let i = 0; i < 6; i++) {
+        let id = Math.floor(Math.random() * 70) + 1;
         try {
-            const result = await fetch(ALBUM_URL + id, {
+            const result = await fetch(ARTIST_URL + id, {
                 headers: {
                     authorization: TOKEN,
                 },
@@ -79,17 +118,49 @@ async function randomFourAlbums() {
 
             if (result.ok) {
                 const data = await result.json();
-                if (data.id) {
-                    console.log("album4 id: ", data.id);
-                    let album = new Albums(data.title, data.cover_big, data.label, data.tracklist, data.artist.name, data.release_date, data.nb_tracks, data.duration);
-                    arrFourAlbums.push(album);
+                if (data.id && data.nb_album) {
+                    console.log("Artist id: ", data.id);
+                    let artist = new Artists(data.name, data.picture_big, data.nb_fan, data.tracklist);
+                    arrArtists.push(artist);
                 } else {
                     i--;
-                    console.log("id non valido");
+                    console.log("id non valido, o artista senza canzoni");
                 }
             } else {
                 i--;
-                throw new Error('Error getting album');
+                throw new Error('Error getting artist');
+            }
+        } catch (err) {
+            console.log("Errore durante la richiesta:", err);
+        }
+    }
+
+    console.log(arrArtists);
+    await fetchTracks();
+    printSecondCards();
+}
+
+//faccio la fetch delle traccie di ogni artista, cosi da ricavarmi le immagini delle canzoni e tutti i dati utili per la pagina artist
+async function fetchTracks() {
+    for (let i = 0; i < arrArtists.length; i++) {
+        const linkTracks = arrArtists[i].tracklistURL;
+        try {
+            const result = await fetch(linkTracks, {
+                headers: {
+                    authorization: TOKEN,
+                },
+            });
+
+            if (result.ok) {
+                const data = await result.json();
+                const datas = data.data;
+                const arrTracksOneArtist = [];
+                datas.forEach((element) => {
+                    arrTracksOneArtist.push(new Tracks(element.title, element.album.cover_big, element.rank, element.duration));
+                });
+                arrTracks.push(arrTracksOneArtist);
+            } else {
+                throw new Error('Error getting track');
             }
         } catch (err) {
             console.log("Errore durante la richiesta:", err);
@@ -98,7 +169,6 @@ async function randomFourAlbums() {
 }
 
 async function randomAlbum() {
-    let i = 0;
     for (let i = 0; i < 11; i++) {
         let id = Math.floor(Math.random() * 150) + 500000;
         try {
@@ -173,16 +243,16 @@ function printCards() {
     artist.innerText = artistAlbum;
 }
 
-function listenersBtn(){
-    for(let i=0; i<6; i++){
-        cards[i].addEventListener("click", () => handleAlbumClick(arrSixAlbums[i]));
+function listenersBtn() {
+    for (let i = 0; i < 6; i++) {
+        cards[i].addEventListener("click", () => handleAlbumClick(arrArtists[i]));
     }
-    for(let i=6; i<11; i++){
-        cards[i].addEventListener("click", () => handleAlbumClick(arrAlbums[i-6]));
-        cards[i+5].addEventListener("click", () => handleAlbumClick(arrAlbums[i-6]));
+    for (let i = 6; i < 11; i++) {
+        cards[i].addEventListener("click", () => handleAlbumClick(arrAlbums[i - 6]));
+        cards[i + 5].addEventListener("click", () => handleAlbumClick(arrAlbums[i - 6]));
     }
-    for(let i=16; i<21; i++){
-        cards[i].addEventListener("click", () => handleAlbumClick(arrAlbums[i-11]));
+    for (let i = 16; i < 21; i++) {
+        cards[i].addEventListener("click", () => handleAlbumClick(arrAlbums[i - 11]));
     }
 }
 
